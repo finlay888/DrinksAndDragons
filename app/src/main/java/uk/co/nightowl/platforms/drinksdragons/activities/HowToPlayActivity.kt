f250b2ebbd7dbf330153c93226caede903e2cdaa
finlay888 <finlayjmm@gmail.com>
2020-11-22 01:18:39 +0000

    Tutorial page creation

---
+++ app/src/main/java/uk/co/nightowl/platforms/drinksdragons/activities/HowToPlayActivity.kt
@@ -0,0 +1,12 @@
+package uk.co.nightowl.platforms.drinksdragons.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uk.co.nightowl.platforms.drinksdragons.R
+
+class HowToPlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how_to_play)
+    }
+}
